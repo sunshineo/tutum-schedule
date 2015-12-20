1c780eee0b9e4ffacb42831168b27b5095145bf9
Gordon Sun <sunshineo@gmail.com>
2015-12-20 15:37:43 -0800

    Use python logging and log notes of start/stop service.

--- tutum-schedule.py
+++ tutum-schedule.py
@@ -2,6 +2,15 @@ import functools
 import schedule
 import time
 import tutum
+import logging
+import sys
+
+log = logging.getLogger(__name__)
+out_hdlr = logging.StreamHandler(sys.stdout)
+out_hdlr.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+out_hdlr.setLevel(logging.INFO)
+log.addHandler(out_hdlr)
+log.setLevel(logging.INFO)
 
 def catch_exceptions(job_func):
     @functools.wraps(job_func)
@@ -10,24 +19,34 @@ def catch_exceptions(job_func):
             job_func(*args, **kwargs)
         except:
             import traceback
-            print(traceback.format_exc())
+            log.warn(traceback.format_exc())
     return wrapper
 
 @catch_exceptions
-def start_service(uuid):
+def start_service(uuid, note):
     """
     Wrapper function to start a stopped Tutum Service by its UUID.
     """
+    log.info("Start service: " + note)
+    log.info("Get service by uuid: " + uuid)
     service = tutum.Service.fetch(uuid)
+    log.info("Successfully got the service.")
+    log.info("Starting the service.")
     service.start()
+    log.info("Successfully started the service.")
 
 @catch_exceptions
-def stop_service(uuid):
+def stop_service(uuid, note):
     """
     Wrapper function to stopped a Tutum Service by its UUID.
     """
+    log.info("Stop service: " + note)
+    log.info("Get service by uuid: " + uuid)
     service = tutum.Service.fetch(uuid)
+    log.info("Successfully got the service.")
+    log.info("Stopping the service.")
     service.stop()
+    log.info("Successfully stopped the service.")
 
 @catch_exceptions
 def create_service(**kwargs):
@@ -61,19 +80,19 @@ if __name__ == "__main__":
     """
 
     # gordonsun.me mariadb backup
-    schedule.every().day.do(start_service, 'fbb5646e-f5b6-428d-8846-aff09469349f')
+    schedule.every().day.do(start_service, 'fbb5646e-f5b6-428d-8846-aff09469349f', 'gordonsun.me mariadb backup daily')
 
     # craigsmenu prod to dev sync
-    schedule.every().day.do(start_service, '19ba2002-8a97-414c-a5da-c8fd3d42412a')
+    schedule.every().day.do(start_service, '19ba2002-8a97-414c-a5da-c8fd3d42412a', 'craigsmenu prod to dev sync daily')
     # craigsmenu prod backup
-    schedule.every().day.do(start_service, 'eb3aeb88-c363-4bad-8060-7fbcb5d50ca7')
+    schedule.every().day.do(start_service, 'eb3aeb88-c363-4bad-8060-7fbcb5d50ca7', 'craigsmenu prod backup daily')
 
     # nolostdogs.org crawler JavaScript
     # This service occationally just cannot stop. So we try to stop it every time before we try to start it.
-    schedule.every(15).minutes.do(stop_service, 'a4a376be-24df-4668-91fd-a0ba56eacc70')
-    schedule.every(15).minutes.do(start_service, 'a4a376be-24df-4668-91fd-a0ba56eacc70')
+    schedule.every(15).minutes.do(stop_service, 'a4a376be-24df-4668-91fd-a0ba56eacc70', 'nld-crawler-js stop (may fail) every 15 minutes')
+    schedule.every(15).minutes.do(start_service, 'a4a376be-24df-4668-91fd-a0ba56eacc70', 'nld-crawler-js start every 15 minutes')
     # nolostdogs.org crawler Python
-    schedule.every(15).minutes.do(start_service, 'a4a49883-82c1-4e9f-bdcd-e38ec78787cb')
+    schedule.every(15).minutes.do(start_service, 'a4a49883-82c1-4e9f-bdcd-e38ec78787cb', 'nld-crawler-python every 15 minutes')
 
     while True:
         schedule.run_pending()
